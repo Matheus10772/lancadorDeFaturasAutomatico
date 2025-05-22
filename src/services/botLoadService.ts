a1c717b93933f90361d5a183dfecd8bda635fd4b
Matheus10772 <matheusdecastrooliveira1074@gmail.com>
2025-05-22 16:34:06 -0300

    Commit incremental: Adição na implementação do serviço de gerenciamento do bot.

--- src/services/botLoadService.ts
+++ src/services/botLoadService.ts
@@ -1,14 +1,28 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Context, Markup, Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
+import CSVParserClass from './loadAndParseCSVService';
+import { Banco, Enconding } from './loadAndParseCSVService';
+import https from 'https';
 import { Update } from 'telegraf/typings/core/types/typegram';
 import { message } from 'telegraf/filters';
+import { session } from 'telegraf';
 
-const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_TOKEN!)
+// Extend the context to include session
+import { Context as TelegrafContext } from 'telegraf';
+import path from 'path';
+
+interface MyContext extends TelegrafContext {
+  session?: UserSessionData;
+}
+
+const bot: Telegraf<MyContext> = new Telegraf(process.env.BOT_TOKEN!)
+const csvParser: CSVParserClass = new CSVParserClass();
 
 let mes: string;
 let ano: string;
+let banco: Banco;
 
 interface sequenceOfOptionsButtons {
   nome: string;
@@ -18,8 +32,8 @@ interface sequenceOfOptionsButtons {
 const listaDeMeses: sequenceOfOptionsButtons[] = [
   { nome: 'Janeiro', valor: 'mes_janeiro' },
   { nome: 'Fevereiro', valor: 'mes_fevereiro' },
-  { nome: 'Março', valor: 'mes_marco' },
-  { nome: 'Abril', valor: 'mes_abri' },
+  { nome: 'Março', valor: 'mes_março' },
+  { nome: 'Abril', valor: 'mes_abril' },
   { nome: 'Maio', valor: 'mes_maio' },
   { nome: 'Junho', valor: 'mes_junho' },
   { nome: 'Julho', valor: 'mes_julho' },
@@ -29,6 +43,19 @@ const listaDeMeses: sequenceOfOptionsButtons[] = [
   { nome: 'Novembro', valor: 'mes_novembro' },
   { nome: 'Dezembro', valor: 'mes_dezembro' }
 ];
+
+const listaDeBancos: sequenceOfOptionsButtons[] = [
+  { nome: 'Nubank', valor: 'banco_nubank' },
+  { nome: 'Itaú', valor: 'banco_itau' },
+  { nome: 'PicPay', valor: 'banco_picpay' },
+  { nome: 'NossoPay', valor: 'banco_nossopay' },
+  { nome: 'Banco do Brasil', valor: 'banco_bancodobrasil' }
+];
+
+interface UserSessionData {
+  AguardandoCapturaDaProximaResposta: boolean;
+  AcaoAnterior: string;
+}
 
 function makeSequencButtons(Conjunto: sequenceOfOptionsButtons[], quantiadePorLinha: number): any{
 
@@ -41,12 +68,68 @@ function makeSequencButtons(Conjunto: sequenceOfOptionsButtons[], quantiadePorLi
 
 }
 
+async function checkActionToDo(userInput: any, session: UserSessionData): Promise<string> {
+  try {
+    switch (session.AcaoAnterior) {
+    case 'escolhaAno':
+       if (/^\d{4}$/.test(userInput)) {
+        ano = userInput;
+        return "Ano definido com sucesso: " + ano;
+      } else {
+        return "Formato de ano inválido. Por favor, digite um ano válido (ex: 2025).";
+      }
+      break;
+    case 'enviarPlanilha':
+      try {
+        // Verifica se é CSV
+        if (!userInput.file_name?.endsWith('.csv')) {
+          return '❌ Por favor, envie um arquivo com extensão .csv';
+        } else {
+          const resultMessage: string = await csvParser.writeCSVtoFile(banco, mes, ano, userInput.file_id);
+
+          return resultMessage;
+        }  
+      } catch (error) {
+        console.error('Erro ao processar o arquivo:', error);
+        return '❌ Erro ao processar o arquivo. Por favor, tente novamente.';
+      }
+      
+
+      
+
+  
+    default:
+      return "Ação não reconhecida.";
+      break;
+    }  
+  } catch (error) {
+    throw new Error('Erro ao verificar a ação a ser realizada: ' + error);
+  }
+  
+}
+
+bot.hears(/.*/, async (ctx) => {
+  const session = ctx.session as UserSessionData;
+
+  try {
+    if (session.AguardandoCapturaDaProximaResposta) {
+     const message: string =  await checkActionToDo(ctx.message.text, session);
+     session.AguardandoCapturaDaProximaResposta = false; // Reseta a flag após o processamento
+     ctx.reply(message);
+  }
+  } catch (error) {
+    console.error('Erro ao processar a mensagem:', error);
+    session.AguardandoCapturaDaProximaResposta = false; // Reseta a flag após o erro
+    ctx.reply('Ocorreu um erro ao processar sua mensagem. Por favor, digite /start e tente novamente.');
+  }
+});
 
 
 bot.command('start', async (ctx) => {
     await ctx.reply('Escolha uma opção:', Markup.inlineKeyboard([
     [Markup.button.callback('Escolher Mês', 'escolhaMes')],
     [Markup.button.callback('Escolher Ano', 'escolhaAno')],
+    [Markup.button.callback('Escolher Banco', 'escolhaBanco')],
     [Markup.button.callback('Enviar Planilha CSV preenchida', 'enviarPlanilha')],
     [Markup.button.callback('Baixar Modelo de Planilha CSV', 'baixarModelo')],
     [Markup.button.callback('Processar Planilha CSV para o Mês e Ano Definidos', 'processarPlanilha')],
@@ -54,25 +137,101 @@ bot.command('start', async (ctx) => {
   ]));
 });
 
-// Responder ao botão "Opção 1"
+
 bot.action('escolhaMes', async (ctx) => {
   await ctx.answerCbQuery(); // remove o loading
-  
-
   await ctx.reply( 'Escolha o Mês que deseja selecionar: ', Markup.inlineKeyboard( makeSequencButtons(listaDeMeses, 2) ) );
 });
 
-// Responder ao botão "Opção 2"
-bot.action('escolhaAno', async (ctx) => {
-  await ctx.answerCbQuery();
-  await ctx.reply('Digite o ano desejado:', );
+bot.action(/^mes_(\w+)$/, async (ctx) => {
+  await ctx.answerCbQuery(); //Remove o loading
+  
+  const nomeDoMes: string = ctx.match[1]; // "janeiro", "fevereiro", etc.
+
+  mes = nomeDoMes;
+
+  await ctx.reply(`Mês definido com sucesso: ${nomeDoMes}`);
+
 });
 
-// Responder ao botão "Ajuda"
-bot.action('ajuda', async (ctx) => {
-  await ctx.answerCbQuery();
-  await ctx.reply('Este é um bot de exemplo. Use /start para recomeçar.');
+bot.action('escolhaBanco', async (ctx) => {
+  await ctx.answerCbQuery(); // remove o loading
+  await ctx.reply( 'Escolha o Banco que deseja selecionar: ', Markup.inlineKeyboard( makeSequencButtons(listaDeBancos, 2) ) );
 });
+
+bot.action(/^banco_(\w+)$/, async (ctx) => {
+  await ctx.answerCbQuery(); //Remove o loading
+  
+  const nomeBanco: string = ctx.match[1]; // "itau", "nubank", etc.
+  banco = Banco[nomeBanco.toLocaleUpperCase() as keyof typeof Banco]; // Converte para o enum Banco usando a string
+
+  await ctx.reply(`Banco definido com sucesso: ${nomeBanco}`);
+
+});
+
+
+
+bot.action('escolhaAno', async (ctx) => {
+  const session = ctx.session as UserSessionData;
+  session.AguardandoCapturaDaProximaResposta = true;
+  session.AcaoAnterior = 'escolhaAno';
+
+
+  await ctx.answerCbQuery();
+  await ctx.reply('Digite o ano desejado. Exemplo: 2025', );
+});
+
+
+bot.use((ctx, next) => {
+  // Inicializa a sessão como um objeto tipado
+  ctx.session = ctx.session ?? {AguardandoCapturaDaProximaResposta: true} as UserSessionData;
+  return next();
+});
+
+
+bot.action('enviarPlanilha', async (ctx) => {
+  const session = ctx.session as UserSessionData;
+  session.AguardandoCapturaDaProximaResposta = true;
+  session.AcaoAnterior = 'enviarPlanilha';
+
+
+  await ctx.answerCbQuery(); // remove o loading
+  await ctx.reply( '📄 Por favor, envie o arquivo da planilha no formato CSV. O arquivo será armazenado com base no banco selecionado.');
+
+});
+
+bot.on('document', async (ctx) => {
+  const file = ctx.message.document;
+  const session = ctx.session as UserSessionData;
+
+  try {
+    if(session.AguardandoCapturaDaProximaResposta) {
+      const resultMessage: string = await checkActionToDo(file, session);
+      session.AguardandoCapturaDaProximaResposta = false; // Reseta a flag após o processamento
+      ctx.reply(resultMessage);
+    }  
+  } catch (error) {
+    console.error('Erro ao processar o arquivo:', error);
+    session.AguardandoCapturaDaProximaResposta = false; // Reseta a flag após o erro
+    ctx.reply('❌ Erro ao processar o arquivo. Por favor, tente novamente.'); 
+  }
+  
+});
+
+bot.action('baixarModelo', async (ctx) => {
+  await ctx.answerCbQuery(); // remove o loading
+
+  try {
+    const filePath: string = csvParser.getExampleCSV();  
+    await ctx.replyWithDocument({ source: filePath }, { caption: 'Modelo de planilha CSV' });
+  } catch (error) {
+    console.error('Erro ao baixar o modelo de planilha:', error);
+    await ctx.reply('❌ Erro ao baixar o modelo de planilha. Por favor, tente novamente.');
+  }
+
+});
+
+
 
 
 
--- src/services/loadAndParseCSVService.ts
+++ src/services/loadAndParseCSVService.ts
@@ -2,7 +2,9 @@ import os from 'os';
 import path from 'path';
 import fs, { ReadStream } from 'fs';
 import csv from 'csv-parser';
+import https from 'https';
 import Stream from 'stream';
+import { file } from 'googleapis/build/src/apis/file';
 
 const dataDir: string = path.join(os.homedir(), process.env.INIT_DIR! );
 
@@ -23,6 +25,7 @@ class CSVParser {
     private separator: string;
     private encoding: Enconding;
     private csvSkipLines: number;
+    private exampleCSVPath: string;
 
 
     constructor() {
@@ -30,6 +33,7 @@ class CSVParser {
         this.separator = ';';
         this.encoding = Enconding.UTF8;
         this.csvSkipLines = 1;
+        this.exampleCSVPath = path.join(this.basePath, 'example.csv');
     }
 
 
@@ -56,4 +60,50 @@ class CSVParser {
         return results;
     }
 
+    public async writeCSVtoFile(banco: Banco, mes: string, ano: string,fileLink: string): Promise<string> {
+        let result: string = '';
+
+        const filePath = path.join(this.basePath, banco.toString(), `${mes}/faturaConvertida/fatura.csv`);
+        const fileStream = fs.createWriteStream(filePath, { encoding: 'utf8' });
+
+        const fileURL = new URL(fileLink);
+
+        https.get(fileURL, (response) => {
+            if(response.statusCode !== 200) {
+                result = `❌ Erro ao baixar o arquivo: código ${response.statusCode}`;
+                return result;
+            }
+
+            response.pipe(fileStream);
+
+            fileStream.on('finish', () => {
+                fileStream.close();
+                result = `✅ Arquivo baixado com sucesso: ${filePath}`;
+                return result;
+                
+            });
+
+
+        }).on('error', (error) => {
+            result = `❌ Erro ao baixar o arquivo: ${error.message}`;
+            return result;
+        });
+
+        return result;
+
+    }
+
+    public getExampleCSV(): string {
+    
+        if(!fs.existsSync(this.exampleCSVPath))
+            throw new Error('Arquivo de exemplo não encontrado');    
+        
+        return this.exampleCSVPath;
+    
+            
+    }
+
 }
+
+export default CSVParser;
+export { Banco, Enconding };
